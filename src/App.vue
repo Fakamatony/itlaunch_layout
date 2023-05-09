a8c3c9805e7a74e33382e9d93933354316ecf5c5
fakamatony <irn21bru.official@yandex.ru>
2023-05-09 21:01:35 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template >
+  <div style="height: 100vh; width: 100vw;">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
@@ -16,11 +18,14 @@ export default {
 
 <style>
 #app {
+  width: 100vw;
+  height: 100vh;
+  margin: 0 auto;
+  background-color: #42b983;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import '../src/assets/styles/reset.css'
+import router from '@/router/router';
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.config.productionTip = false
+
+
+app.mount('#app')
